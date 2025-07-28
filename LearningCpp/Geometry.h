4acfe4f81864b015724be03aec2aa6543b14062c
Alvaro Montagud Andres <almonan@outlook.com>
2025-07-28 07:27:17 +0200

    main - Creacion de proyecto.

---
+++ LearningCpp/Geometry.h
@@ -0,0 +1,14 @@
+// #pragma once al parecer es util para evitar que al compilar se puedan llegar
+// a definir clases, funciones y macros que estan nombrados ya en otro 
+// archivo de cabecera.
+// Lo malo es que no es parte de c++, es mejor usar el bloque guard 
+// que si es una directiva de preprocesador nativa de c++.
+#ifndef LearningCpp_Point2D_CPP_GUARD
+#define LearningCpp_Point2D_CPP_GUARD
+class Point2D
+{
+};
+#endif // !LearningCpp_Point2D_CPP_GUARD
+
+
+
