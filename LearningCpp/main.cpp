b518b7fad61a05ee516c01d7556942028f89f49e
Almonan <almonan@outlook.com>
2025-07-31 19:47:24 +0200

    Proyecto modificado para niveles de errores al compilar y version de lenguage cpp

--- LearningCpp/main.cpp
+++ LearningCpp/main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
+
+void foo(int)
+{
+
+}
 // this file contains the 'main' function. Program execution starts here.
 int main() 
 {
+	unsigned int x{ 5 };
+	foo(x);
 	std::cout << "Here is some text";
 	return 0;
 }
