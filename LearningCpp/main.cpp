5648ee299c3124508ca8f079a4b49a7640619a63
Almonan <almonan@outlook.com>
2025-08-12 20:31:00 +0200

    NS-1 Creadas funciones sumas y restas para puntos y vectores.

--- LearningCpp/main.cpp
+++ LearningCpp/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+// Se tiene que crear primero porque es el orden logico de compilacion. Si lo pones despues, no compila poruqe no esta definido antes.
+struct Vector2D
+{
+	double X;
+	double Y;
+};
 
+struct Point2D
+{
+	double X;
+	double Y;
+};
+
+Point2D SumPoints(Point2D point1, Point2D point2);
+
+Vector2D SumVector(Vector2D vector1, Vector2D vector2);
+
+Point2D RestPoints(Point2D point1, Point2D point2);
+
+Vector2D RestVector(Vector2D vector1, Vector2D vector2);
 
 int main()
 {
@@ -13,28 +32,78 @@ int main()
 
 	Point2D point1{ .X {2}, .Y {4.5} };
 	Point2D point2{ .X {4.78}, .Y {6.7} };
-	Point2D point3{ .X {0.32}, .Y {5.5} };
-	Point2D point4{ .X {8.99}, .Y {9.9} };
+	[[maybe_unused]] Point2D point3{ .X {0.32}, .Y {5.5} };
+	[[maybe_unused]] Point2D point4{ .X {8.99}, .Y {9.9} };
 
+	// perform operations over the points.
+
+	SumPoints(point1, point2);
+	RestPoints(point1, point2);
 
 	// perform operations over the vectors.
 
-	// perform operations over the points.
+	SumVector(vector1, vector2);
+	RestVector(vector1, vector2);
 
 	// perform operations between points and vectors.
 }
 
-struct Vector2D
+Vector2D SumVector(Vector2D vector1, Vector2D vector2)
 {
-	double X;
-	double Y;
-};
+	Vector2D sumOfVector{ .X{}, .Y{} };
 
-struct Point2D
+	sumOfVector.X = vector1.X + vector2.X;
+	sumOfVector.Y = vector1.Y + vector2.Y;
+
+	std::cout << "vector1 X: " << vector1.X << " vector1 Y: " << vector1.Y << std::endl;
+	std::cout << "vector2 X: " << vector2.X << " vector2 Y: " << vector2.Y << std::endl;
+	std::cout << "sumOfVector X: " << sumOfVector.X << " sumOfVector Y: " << sumOfVector.Y << std::endl;
+
+	return sumOfVector;
+}
+
+Point2D SumPoints(Point2D point1, Point2D point2)
 {
-	double X;
-	double Y;
-};
+	Point2D sumOfPoints{ .X{}, .Y{} };
+
+	sumOfPoints.X = point1.X + point2.X;
+	sumOfPoints.Y = point1.Y + point2.Y;
+
+	std::cout << "point1 X: " << point1.X << " point1 Y: " << point1.Y << std::endl;
+	std::cout << "point2 X: " << point2.X << " point2 Y: " << point2.Y << std::endl;
+	std::cout << "sumOfPoints X: " << sumOfPoints.X << " sumOfPoints Y: " << sumOfPoints.Y << std::endl;
+
+	return sumOfPoints;
+}
+
+Vector2D RestVector(Vector2D vector1, Vector2D vector2)
+{
+	Vector2D restOfVector{ .X{}, .Y{} };
+
+	restOfVector.X = vector1.X - vector2.X;
+	restOfVector.Y = vector1.Y - vector2.Y;
+
+	std::cout << "vector1 X: " << vector1.X << " vector1 Y: " << vector1.Y << std::endl;
+	std::cout << "vector2 X: " << vector2.X << " vector2 Y: " << vector2.Y << std::endl;
+	std::cout << "restOfVector X: " << restOfVector.X << " restOfVector Y: " << restOfVector.Y << std::endl;
+
+	return restOfVector;
+}
+
+Point2D RestPoints(Point2D point1, Point2D point2)
+{
+	Point2D restOfPoints{ .X{}, .Y{} };
+
+	restOfPoints.X = point1.X - point2.X;
+	restOfPoints.Y = point1.Y - point2.Y;
+
+	std::cout << "point1 X: " << point1.X << " point1 Y: " << point1.Y << std::endl;
+	std::cout << "point2 X: " << point2.X << " point2 Y: " << point2.Y << std::endl;
+	std::cout << "restOfPoints X: " << restOfPoints.X << " restOfPoints Y: " << restOfPoints.Y << std::endl;
+
+	return restOfPoints;
+}
+
 
 void initializayion()
 {
